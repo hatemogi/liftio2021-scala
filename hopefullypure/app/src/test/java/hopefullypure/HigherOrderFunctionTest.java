db668a0b2102100f9ee81c72628b30edd95c824f
Daehyun Kim <hatemogi@gmail.com>
2021-10-11 16:33:38 +0900

    자바 고차함수 예제 추가

---
+++ hopefullypure/app/src/test/java/hopefullypure/HigherOrderFunctionTest.java
@@ -0,0 +1,16 @@
+package hopefullypure;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class HigherOrderFunctionTest extends HigherOrderFunction {
+    @Test
+    public void examples() {
+        assertEquals(4, (int)square.apply(2));
+        assertEquals(triple.apply(2), multiple(3).apply(2));
+    }
+
+}
